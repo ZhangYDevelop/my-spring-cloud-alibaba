803b37ec5a571d908a45135da98631ca7bd67c7c
ZhangYDevelop <2389311433@qq.com>
2020-06-02 17:37:58 +0800

    gate-way code init

---
+++ gate-way/src/main/java/com/zy/dubbo/gateway/GateWayApplicaton.java
@@ -0,0 +1,17 @@
+package com.zy.dubbo.gateway;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableDubbo
+public class GateWayApplicaton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplicaton.class, args);
+    }
+}
