24f0266d3973d95decdd1d499eaf1bc0e5dd5856
ZhangYDevelop <2389311433@qq.com>
2020-06-03 14:25:43 +0800

    通用工具类

---
+++ utils/src/main/java/com/zy/alibaba/utils/ResponseBody.java
@@ -0,0 +1,41 @@
+package com.zy.alibaba.utils;
+
+/**
+ * 响应
+ * @param <T>
+ */
+public class  ResponseBody<T> {
+
+    private String msg = "数据处理成功";
+
+    private T Data;
+
+    private boolean success;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public ResponseBody setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public T getData() {
+        return Data;
+    }
+
+    public ResponseBody setData(T data) {
+        Data = data;
+        return this;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public ResponseBody setSuccess(boolean success) {
+        this.success = success;
+        return this;
+    }
+}
