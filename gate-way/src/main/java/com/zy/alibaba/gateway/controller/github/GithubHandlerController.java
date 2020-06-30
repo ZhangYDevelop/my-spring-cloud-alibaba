f8c79150d2bb909af41afa766ea783b106aa7543
ZhangYDevelop <2389311433@qq.com>
2020-06-30 22:05:18 +0800

    网关集成github第三方登录

--- gate-way/src/main/java/com/zy/alibaba/gateway/controller/github/GithubHandlerController.java
+++ gate-way/src/main/java/com/zy/alibaba/gateway/controller/github/GithubHandlerController.java
@@ -1,15 +1,12 @@
 package com.zy.alibaba.gateway.controller.github;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zy.alibaba.gateway.conf.AssesTokenProperties;
-import com.zy.alibaba.gateway.conf.GitHubProvider;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.util.MapUtils;
 
 import java.io.IOException;
 
