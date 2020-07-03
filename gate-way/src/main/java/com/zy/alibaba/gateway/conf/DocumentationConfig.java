e7a2f5cd5da858bddb449cda3ec3158b5076b7b9
ZhangYDevelop <2389311433@qq.com>
2020-07-03 17:44:04 +0800

    网关重构

--- gate-way/src/main/java/com/zy/alibaba/gateway/conf/DocumentationConfig.java
+++ gate-way/src/main/java/com/zy/alibaba/gateway/conf/DocumentationConfig.java
@@ -1,42 +1,42 @@
-//package com.zy.alibaba.gateway.conf;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.gateway.route.Route;
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.context.annotation.Configuration;
-//import reactor.core.publisher.Flux;
-//import springfox.documentation.swagger.web.SwaggerResource;
-//import springfox.documentation.swagger.web.SwaggerResourcesProvider;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Configuration
-//public class DocumentationConfig implements SwaggerResourcesProvider {
-////    @Autowired
-////    private  RouteLocator routeLocator;
-//    // 自动获取系统配置的路由资源集合
-//    @Override
-//    public List<SwaggerResource> get() {
-//        List<SwaggerResource> resources = new ArrayList<>();
-////        Flux<Route> routes = routeLocator.getRoutes();
-////        for (List<Route> routeList : routes.buffer().toIterable()) {
-////            routeList.forEach(route -> {
-////                resources.add(swaggerResource(route.getId(),
-////                        route.getUri().getPath().replace("**", "v2/api-docs"), ""));
-////            });
-////        }
-//
-//
-//        return resources;
-//    }
-//
-//    // 获取对应的路由资源
-//    private SwaggerResource swaggerResource(String name, String location, String version) {
-//        SwaggerResource swaggerResource = new SwaggerResource();
-//        swaggerResource.setName(name);
-//        swaggerResource.setLocation(location);
-//        swaggerResource.setSwaggerVersion(version);
-//        return swaggerResource;
-//    }
-//}
+package com.zy.alibaba.gateway.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class DocumentationConfig implements SwaggerResourcesProvider {
+//    @Autowired
+//    private  RouteLocator routeLocator;
+    // 自动获取系统配置的路由资源集合
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+//        Flux<Route> routes = routeLocator.getRoutes();
+//        for (List<Route> routeList : routes.buffer().toIterable()) {
+//            routeList.forEach(route -> {
+//                resources.add(swaggerResource(route.getId(),
+//                        route.getUri().getPath().replace("**", "v2/api-docs"), ""));
+//            });
+//        }
+
+
+        return resources;
+    }
+
+    // 获取对应的路由资源
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
